ff6ec287af30cce0f59556d007145a53d8637004
himanshusharma98 <97788582+himanshusharma98@users.noreply.github.com>
2022-04-06 21:01:41 +0530

    first commit

---
+++ input/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Enter Your Username:-");
+            Console.WriteLine("Enter Your Email:-");
+            string userName = Console.ReadLine();
+            string email = Console.ReadLine();  
+            Console.WriteLine("Username is:-" + userName);
+            Console.WriteLine("Email is:-" + email);
+            Console.WriteLine("---------------------");
+        }
+    }
+}
